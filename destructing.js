700193bbc11c0d1a6a516b34ff55900d4641f047
Raizer Varela <raizer.varela@gmail.com>
2021-03-31 11:54:02 -0300

    Exercícios de fundamentos

---
+++ destructing.js
@@ -0,0 +1,20 @@
+const pessoa = {
+    nome: 'Ana',
+    idade: 5,
+    endereco: {
+        logradouro: 'Rua ABC',
+        numero: 1000
+    }
+}
+
+const {nome, idade} = pessoa
+console.log(nome, idade)
+
+const {nome: n, idade: i} = pessoa
+console.log(n, i)
+
+//const {nome, sobrenome = true} = pessoa
+//console.log(nome, sobrenome)
+
+const {endereco:{logradouro, numero}} = pessoa
+console.log(logradouro, numero)
