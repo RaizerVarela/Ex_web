16961de3d2b7921ab1e50d788ba61e6e6083d209
Raizer Varela <raizer.varela@gmail.com>
2021-04-02 20:45:50 -0300

    Exercícios de fundamentos

---
+++ fundamentos/objeto.js
@@ -0,0 +1,16 @@
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4000.98
+
+console.log(prod1.nome, prod1.preco)
+
+const prod2 = {
+    nome : 'S10',
+    preco : 6000.02
+}
+
+console.log(prod2.nome, prod2.preco)
+
+prod2.nome = 'S20'
+
+console.log(prod2.nome)
---
+++ fundamentos/relacionais.js
@@ -0,0 +1,12 @@
+console.log('1' == 1)
+console.log('1' === 1)
+console.log('1' === '1')
+
+const d1 = new Date(0)
+const d2 = new Date(0)
+console.log(d1 === d2)
+console.log(d1 == d2)
+console.log(d1.getTime === d2.getTime)
+console.log(d1.getTime == d2.getTime)
+
+console.log('1' === '2')
