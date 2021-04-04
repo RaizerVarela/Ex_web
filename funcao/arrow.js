df97d380924e698e16eb593b304c13d9efcbf5ec
Raizer Varela <raizer.varela@gmail.com>
2021-04-04 15:27:21 -0300

    Exercícios função

---
+++ funcao/arrow.js
@@ -0,0 +1,14 @@
+ola = () => 'Olá'
+
+console.log(ola())
+
+function Pessoa(){
+    this.idade = 0
+
+    setInterval(() => {
+        this.idade++
+        console.log(this.idade)
+    }, 1000)
+}
+
+new Pessoa()
